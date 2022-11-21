07868959032889b27fe161affc4d233f7e9aab83
ukjang <bedro4339@gmail.com>
2022-11-21 10:28:14 +0900

    naver_news_drop_duplicate

--- 01_gathering_data/05_naver_news/contextfactory.py
+++
@@ -1,9 +0,0 @@
-from scrapy.core.downloader.contextfactory import ScrapyClientContextFactory
-
-
-class LegacyConnectContextFactory(ScrapyClientContextFactory):
-
-    def getContext(self, hostname=None, port=None):
-        ctx = self.getCertificateOptions().getContext()
-        ctx.set_options(0x4)
-        return ctx
